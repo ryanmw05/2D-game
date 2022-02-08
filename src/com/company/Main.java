7b77c247436021983ba3a1799d3f57fa357bf848
LewisZ497 <lz212344@reigate.ac.uk>
2022-02-08 16:04:54 +0000

    removed junk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        JFrame f = new JFrame("A JFrame");
+        f.setSize(250, 250);
+        f.setLocation(300,200);
+
+        f.setVisible(true);
+
     }
 }
