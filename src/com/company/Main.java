d414a96ac10e331b8486b3bcf507114ee223f4bd
LewisZ497 <lz212344@reigate.ac.uk>
2022-02-11 10:19:08 +0000

    remove "write code here" bit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         JFrame frame = new JFrame("A JFrame");
         frame.setSize(250, 250);
         frame.setLocation(300,200);
